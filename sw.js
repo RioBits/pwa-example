68b707d337e5c3963134e6e6ad20f87c4b15e15b
riobits <23579928+RioBits@users.noreply.github.com>
2021-11-08 23:51:40 +0300

    initial commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+// install event
+self.addEventListener('install', (event) => {
+  console.log('service worker has been installed')
+})
+
+// activate event
+self.addEventListener('activate', (event) => {
+  console.log('service worker has been activated')
+})
+
+// fetch event
+self.addEventListener('fetch', (event) => {
+  console.log('fetch event', event)
+})
