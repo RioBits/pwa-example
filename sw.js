20f453568f04d847fd50d36a49ebaa4c7e925e63
riobits <23579928+RioBits@users.noreply.github.com>
2021-11-09 23:12:35 +0300

    Added Cache

--- sw.js
+++ sw.js
@@ -1,14 +1,38 @@
+const staticCacheName = 'site-static'
+const assets = ['/', '/index.html', '/js/app.js', 'style.css', 'logo.png']
+
 // install event
 self.addEventListener('install', (event) => {
-  console.log('service worker has been installed')
+  // console.log('service worker has been installed')
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets')
+      cache.addAll(assets)
+    })
+  )
 })
 
 // activate event
 self.addEventListener('activate', (event) => {
   console.log('service worker has been activated')
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      // console.log(keys)
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      )
+    })
+  )
 })
 
 // fetch event
 self.addEventListener('fetch', (event) => {
-  console.log('fetch event', event)
+  // console.log('fetch event', event)
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request)
+    })
+  )
 })
